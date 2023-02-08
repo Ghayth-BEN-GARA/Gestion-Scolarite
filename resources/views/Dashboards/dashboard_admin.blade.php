4aa0d2faf032e06e64f3a2adeecf1e2758c34e8b
Ghayth BenGara <ghayth.bengara.26@gmail.com>
2023-02-08 22:34:47 +0100

    Importer les fichier de template

---
+++ resources/views/Dashboards/dashboard_admin.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang = "en">
+    <head>
+        @include("Layouts.head_site")
+        <title>Accueil | Université Sesame</title>
+    </head>
+    <body>
+
+        <div class = "rightbar-overlay"></div>
+        @include("Layouts.script_site")
+    </body>
+</html>
---
+++ resources/views/Layouts/head_site.blade.php
@@ -0,0 +1,9 @@
+<meta charset = "UTF-8">
+<meta name = "viewport" content = "width=device-width, initial-scale=1.0">
+<meta http-equiv = "X-UA-Compatible" content="ie=edge">
+<meta http-equiv = "Content-Security-Policy" content = "upgrade-insecure-requests">
+<meta http-equiv = "Content-type" content = "text/html; charset=utf-8"/>
+<link rel = "icon" href = "{{asset('/images/favicon.png')}}">
+<link rel = "stylesheet" href = "{{asset('/css/icons.min.css')}}">
+<link rel = "stylesheet" href = "{{asset('/css/app.min.css')}}" id = "light-style">
+<link rel = "stylesheet" href = "{{asset('/css/app-dark.min.css')}}"  id = "dark-style">
