5262ca0a25cac1819b676efec7d3ae35c0a9dc38
Ghayth BenGara <ghayth.bengara.26@gmail.com>
2023-03-08 09:44:10 +0100

    Compléter la création des séances

--- app/Http/Controllers/SeanceController.php
+++ app/Http/Controllers/SeanceController.php
@@ -35,7 +35,47 @@
         }
 
         public function gestionCreerSeance(Request $request){
-            # code...
+            if($this->verifierSeanceCreerExiste($request->cours_seance, $request->date_seance, $request->heure_debut_seance, $request->heure_fin_seance)){
+                return back()->with("erreur", "Nous sommes désolés de vous dire qu'il y a une autre séance qui est déjà créée en ce jour et ce créneau horaire.");
+            }
+
+            else if($this->verifierSeanceCreerSalleNonDisponibleExiste($request->date_seance, $request->heure_debut_seance, $request->heure_fin_seance, $request->salle_seance)){
+                return back()->with("erreur", "Nous sommes désolés de vous dire que la salle n'est pas disponible dans ce jour et ce créneau horaire.");
+            }
+
+            else if($this->creerSeance($request->cours_seance, $request->salle_seance, $request->date_seance, $request->heure_debut_seance, $request->heure_fin_seance)){
+                return back()->with("success", "Nous sommes très heureux de vous informer que cette nouvelle séance a été créée avec succès.");
+            }
+
+            else{
+                return back()->with("error", "Pour des raisons techniques, vous ne pouvez pas créer cette nouvelle séance pour le moment. Veuillez réessayer plus tard.");
+            }
+        }
+
+        public function verifierSeanceCreerExiste($cours, $date, $debut, $fin){
+            return Seance::where("id_cours", "=", $cours)
+            ->where("date_seance", "=", $date)
+            ->where("heure_debut_seance", "=", $debut)
+            ->where("heure_fin_seance", "=", $fin)
+            ->exists();
+        }
+
+        public function verifierSeanceCreerSalleNonDisponibleExiste($date, $debut, $fin, $salle){
+            return Seance::where("date_seance", "=", $date)
+            ->where("heure_debut_seance", "=", $debut)
+            ->where("heure_fin_seance", "=", $fin) 
+            ->where("id_salle", "=", $salle)
+            ->exists();
+        }
+
+        public function creerSeance($cours, $salle, $date, $debut, $fin){
+            $seance = new Seance();
+            $seance->setDateSeanceAttribute($date);
+            $seance->setHeureDebutSeanceAttribute($debut);
+            $seance->setHeureFinSeanceAttribute($fin);
+            $seance->setIdSalleAttribute($salle);
+            $seance->setIdCoursAttribute($cours);
+            return $seance->save();
         }
     }
 ?>
